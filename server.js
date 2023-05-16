4452dc894d4d67c7c3e1ea70fe09be5d4eb9cec0
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-05-17 00:24:28 +0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
-
+// olesya
+// first
+const app = require('.app');
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+    console.log("server running.use our ip port 3000")
 })
