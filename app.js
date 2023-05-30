efe662291fd276389ea19971b98eb6b25bbfd855
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-05-30 14:20:26 +0300

    fix mistakes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts');
 
@@ -8,9 +9,9 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
 
--- config.js
+++
@@ -1,5 +0,0 @@
-const DB_HOST = 'mongodb+srv://olesya:first@cluster0.dk3nku5.mongodb.net/project-backend?retryWrites=true&w=majority';
-
-module.exports = {
-    DB_HOST,
-}
