768679f35be436bab8afda316a7cb9292f66bd47
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-05-30 20:02:05 +0300

    reload server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const { DB_HOST, PORT } = process.env;
-
+// console.log(process.env);
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
