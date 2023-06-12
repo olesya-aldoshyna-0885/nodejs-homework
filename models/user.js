86ad718280952ba2d7ddaae80c67d14b69e80ea3
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-06-12 23:50:35 +0300

    done register and login

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+    },
+  owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    });
+
+module.exports = model('user', userSchema);
